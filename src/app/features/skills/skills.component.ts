9d939d5ec2002abf15ba577110471a2b4c9d49a8
MaZen <MazenAnter>
2026-07-22 01:35:03 +0300

    updated skills section

--- src/app/features/skills/skills.component.ts
+++ src/app/features/skills/skills.component.ts
@@ -26,29 +26,33 @@ export class SkillsComponent {
       skills:[
         {name:"C#"},
         {name:"ASP.NET Core Web API"},
-        {name:"MVC"}
+        {name:"ASP.NET MVC"},
+        {name:"Entity Framework Core"},
+        {name:"LINQ"}
       ]
     },
     {
-      name:"Database & ORM",
+      name:"Database",
       icon:"fa-solid fa-database",
       skills:[
         {name:"SQL Server"},
-        {name:"Entity Framework Core"},
-        {name:"Linq"},
-        {name:"Database Optimization & Indexing"},
+        {name:"Database Design"},
+        {name:"Query Optimization"},
+        {name:"Indexing"},
+   {name:"Database Transactions"},
 
       ],
     },
     {
-      name:"Architecture Patterns & Performance",
+      name:"Architecture Patterns & Design",
       icon:"fa-solid fa-diagram-project",
       skills:[
         {name:"Clean Architecture"},
-        {name:"CQRS (Mediator Pattern)"},
-        {name:"Redis"},
-        {name:"Serilog"},
-        {name:"Rate Limiting"},
+        {name:"CQRS (MediatR)"},
+        {name:"Domain Driven Design (DDD)"},
+        {name:"Repository Pattern"},
+        {name:"Unit Of Work"},
+        {name:"Dependency Injection"}
       ]
     },
     {
@@ -57,9 +61,10 @@ export class SkillsComponent {
       skills:
       [
         {name:"JWT Authentication"},
-        {name:"Role-Based Authorization"},
+        {name:"ASP.NET Core Identity"},
+        {name:"Role-Based Access Control (RBAC)"},
         {name:"IP Whitelisting"},
-        {name:"Api Key Management"},
+        {name:"API Key Management"},
       ]
     },
     {
@@ -74,24 +79,47 @@ export class SkillsComponent {
       ]
     },
     {
+  name:"Testing",
+  icon:"fa-solid fa-vial",
+  skills:[
+      {name:"xUnit"},
+      {name:"Moq"},
+      {name:"FluentAssertions"},
+      {name:"Unit Testing"},
+      {name:"AAA Pattern"},
+  ]
+},
+    {
       name:"DevOps & Version Control",
       icon:"fa-solid fa-code-branch",
       skills:
       [
-        {name:"Git"},
-        {name:"Docker Basics"},
-        {name:"Swagger/OpenAPI"},
-        {name:"CI/CD Pipelines"},
+   {name:"Git"},
+      {name:"GitHub"},
+      {name:"GitHub Actions"},
+      {name:"Docker"},
+      {name:"Docker Compose"},
+      {name:"CI/CD"},
+      {name:"Swagger / OpenAPI"},
       ]
     },
+    {name:"API Development",
+     icon:"fa-solid fa-desktop",
+     skills:[
+      {name:"REST APIs"},
+      {name:"Third-Party API Integrations"},
+      {name:"Webhooks"},
+      {name:"IHttpClientFactory"},
+  
+     ]},
     {name:"Frontend",
      icon:"fa-solid fa-desktop",
      skills:[
       {name:"HTML"},
       {name:"CSS"},
       {name:"JavaScript"},
-      {name:"Angular basics"},
-      {name:"TypeScript basics"},
+      {name:"Angular"},
+      {name:"TypeScript"},
      ]}
   ];
 }
